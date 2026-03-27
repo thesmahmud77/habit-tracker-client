72388a4786f3c17796eaa0e4430fdb7af451236a
thesmahmud77 <mahmudsazzad77@gmail.com>
2026-03-27 21:22:08 +0600

    Auth Update

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,12 +1,100 @@
 import React from "react";
 import MainContainer from "../../Container/MainContainer";
 import NavMenu from "../../Components/NavMenu";
+import Footer from "../../Components/Footer";
+import { Link } from "react-router";
 
 const Login = () => {
   return (
     <MainContainer>
       <NavMenu></NavMenu>
-      <h1>Login</h1>
+      <div className=" bg-amber-400 flex items-center justify-center min-h-screen flex-col">
+        <form action="">
+          <fieldset className="fieldset">
+            <div className="Name">
+              <label className="name">Name</label>
+              <input
+                name="name"
+                type="text"
+                className="input"
+                placeholder="Name"
+              />
+            </div>
+            <div className="email">
+              <label className="email">Email</label>
+              <input
+                name="email"
+                type="email"
+                className="input"
+                placeholder="Email"
+              />
+            </div>
+            <div className="photo-url">
+              <label className="label">Email</label>
+              <input
+                name="photo"
+                type="text"
+                className="input"
+                placeholder="Photo-Url"
+              />
+            </div>
+            <div className="password">
+              <label className="label">Password</label>
+              <input
+                name="password"
+                type="password"
+                className="input"
+                placeholder="Password"
+              />
+            </div>
+            <div>
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+            <button className="btn btn-neutral mt-1">Register</button>
+          </fieldset>
+        </form>
+        {/* <!-- Google --> */}
+        <button class="btn mt-5 bg-white text-black border-[#e5e5e5] w-[260px]">
+          <svg
+            aria-label="Google logo"
+            width="16"
+            height="16"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+          >
+            <g>
+              <path d="m0 0H512V512H0" fill="#fff"></path>
+              <path
+                fill="#34a853"
+                d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+              ></path>
+              <path
+                fill="#4285f4"
+                d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+              ></path>
+              <path
+                fill="#fbbc02"
+                d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+              ></path>
+              <path
+                fill="#ea4335"
+                d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+              ></path>
+            </g>
+          </svg>
+          Login with Google
+        </button>
+        <p>
+          if you dont have account!!{" "}
+          <span>
+            <Link className="text-primary" to={"/signup"}>
+              {" "}
+              Please Register
+            </Link>
+          </span>
+        </p>
+      </div>
+      <Footer></Footer>
     </MainContainer>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,42 @@
 import React from "react";
 import NavMenu from "../Components/NavMenu";
 import MainContainer from "../Container/MainContainer";
+import Footer from "../Components/Footer";
 
 const Home = () => {
   return (
     <MainContainer>
       <NavMenu></NavMenu>
-      <h1>Home</h1>
+      <section className="hero-banner">
+        <div className="bg-[url('https://images.unsplash.com/photo-1566073771259-6a8506099945')] bg-cover bg-center h-[500px] flex items-center justify-center text-white">
+          <div className="text-center bg-black/50 p-8 rounded">
+            <h1 className="text-4xl font-bold mb-4">
+              Find Your Perfect Stay at Lighthouse
+            </h1>
+            <p className="mb-4">Discover luxury rooms at the best price</p>
+            <button className="bg-blue-600 px-6 py-2 rounded">
+              Explore Rooms
+            </button>
+          </div>
+        </div>
+      </section>
+      <section className="Feacture"></section>
+      <section className="Hotel-Animation"></section>
+      <section className="news-letter">
+        <div className="text-center py-10">
+          <h2 className="text-xl font-bold mb-4">Subscribe</h2>
+
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className="border px-4 py-2 mr-2"
+          />
+          <button className="bg-blue-600 text-white px-4 py-2">
+            Subscribe
+          </button>
+        </div>
+      </section>
+      <Footer></Footer>
     </MainContainer>
   );
 };
