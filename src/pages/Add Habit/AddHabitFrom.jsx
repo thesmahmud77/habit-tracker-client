55c88b635bffc3bcdce04446551799e980739bab
thesmahmud77 <mahmudsazzad77@gmail.com>
2026-03-30 21:52:38 +0600

    Add habit Page Update

--- src/pages/Add Habit/AddHabitFrom.jsx
+++ src/pages/Add Habit/AddHabitFrom.jsx
@@ -1,28 +1,47 @@
 import React, { useContext } from "react";
 import MainContainer from "../../Container/MainContainer";
 import { AuthContext } from "../../Context/AuthContext";
-
-// const { user } = useContext(AuthContext);
+import Swal from "sweetalert2";
 
 export default function AddHabitFrom() {
+  const { user } = useContext(AuthContext);
   const handleSaveHabit = async (e) => {
     e.preventDefault();
     const habitName = e.target.habitName.value;
     const category = e.target.category.value;
     const description = e.target.description.value;
+    const remainderTime = e.target.remainderTime.value;
     // console.log(habitName, category, description);
 
     const newHabitData = {
       habitTitle: habitName,
       category: category,
       description: description,
-      reminderTime: "7PM",
-      //   userEmail: user.email,
-      //   userName: user.UserName,
-      //   userPhoto: user.photoURL,
+      reminderTime: remainderTime,
+      userEmail: user.email,
+      userName: user.UserName,
+      userPhoto: user.photoURL,
       postCreateTime: new Date().toISOString(),
     };
-    console.log(newHabitData);
+    // console.log(newHabitData);
+    const response = await fetch("http://localhost:3000/habits", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(newHabitData),
+    });
+
+    const data = await response.json();
+    if (data.insertedId) {
+      Swal.fire({
+        title: "Habit Added to your Profile",
+        icon: "success", // ← সবুজ success
+      });
+    } else {
+      Swal.fire({
+        title: "Oops...",
+        icon: "error", // ← লাল error
+      });
+    }
   };
 
   return (
@@ -61,10 +80,21 @@ export default function AddHabitFrom() {
               placeholder="Add some details about why you want to build this habit..."
             ></textarea>
           </div>
+          {/* Remainder time */}
+          <div className="flex flex-col items-start justify-center">
+            <label className="block font-medium mb-1">Remainder Time</label>
+            <input
+              name="remainderTime"
+              type="text"
+              placeholder="e.g., Morning 7PM"
+              className="input input-bordered w-full"
+              required
+            />
+          </div>
 
           {/* Action Buttons */}
           <div className="flex flex-col items-center justify-center">
-            <button type="submit" className="btn btn-primar">
+            <button type="submit" className="btn btn-primary w-100">
               Save Habit
             </button>
           </div>
