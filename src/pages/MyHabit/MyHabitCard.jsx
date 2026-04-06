267aad6721cef59da11a8c7b04d00e5a33298477
thesmahmud77 <mahmudsazzad77@gmail.com>
2026-04-06 22:22:54 +0600

    My Habit Complete Button Funcatility

--- src/pages/MyHabit/MyHabitCard.jsx
+++ src/pages/MyHabit/MyHabitCard.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import Swal from "sweetalert2";
 
 const MyHabitCard = ({ tableHabitData }) => {
-  console.log(tableHabitData);
-  const { habitTitle, reminderTime } = tableHabitData;
+  // console.log(tableHabitData);
+  const [completeHabit, setCompleteHabit] = useState(tableHabitData);
+  const { _id, habitTitle, reminderTime, currentStatus } = completeHabit;
+  const handleStatusUpdate = (id) => {
+    fetch(`http://localhost:3000/my-habits/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ currentStatus: "Complete" }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          // রিয়েল-টাইম আপডেট করার জন্য স্টেট ফিল্টার করা
+          setCompleteHabit({ ...completeHabit, currentStatus: "Complete" });
+          Swal.fire({
+            title: "Status updated to Complete!!",
+            icon: "success",
+            draggable: true,
+          });
+        }
+      });
+  };
+
+  const handleDeleteMyHabit = (id) => {
+    fetch(`http://localhost:3000/my-habits/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount > 0) {
+          alert("Deleted!");
+          window.location.reload();
+        }
+      });
+  };
   return (
     <div>
       <tbody className="overflow-x-auto  border border-gray-500 shadow-sm ">
@@ -11,13 +45,24 @@ const MyHabitCard = ({ tableHabitData }) => {
           <td className="col-span-3  text-center">{habitTitle}</td>
           <td className="col-span-2  text-center">{reminderTime}</td>
           <td className="col-span-2  text-center">
-            <p>Incomplete</p>
+            <p>{currentStatus ? currentStatus : "Panding"}</p>
           </td>
           <td className="col-span-2  text-center">
-            <div className="flex items-center justify-between gap-3">
-              <button className="btn btn-outline">Complete</button>
-              <button className="btn btn-outline">Edit</button>
-              <button className="btn btn-outline">Delete</button>
+            <div className="flex items-center justify-between gap-10">
+              <button
+                onClick={() => handleStatusUpdate(_id)}
+                disabled={currentStatus === "Complete"}
+                className="btn btn-outline"
+              >
+                Complete
+              </button>
+
+              <button
+                onClick={() => handleDeleteMyHabit(_id)}
+                className="btn btn-outline"
+              >
+                Delete
+              </button>
             </div>
           </td>
         </tr>
